83023b0b02b63e710a0a47b50c49529223009233
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-11-21 18:26:04 -0200

    changed post return values

--- soap.go
+++ soap.go
@@ -6,13 +6,14 @@ import "bytes"
 import "io"
 
 type Envelope struct {
-	XMLName       xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
-	Xsi           string   `xml:"xmlns:xsi,attr"`
-	Soapenc       string   `xml:"xmlns:soapenc,attr"`
-	Xsd           string   `xml:"xmlns:xsd,attr"`
-	EncodingStyle string   `xml:"soap:encodingStyle,attr"`
-	Soap          string   `xml:"xmlns:soap,attr"`
-	Body          Body
+	XMLName         xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	Xsi             string   `xml:"xmlns:xsi,attr"`
+	Soapenc         string   `xml:"xmlns:soapenc,attr"`
+	Xsd             string   `xml:"xmlns:xsd,attr"`
+	EncodingStyle   string   `xml:"soap:encodingStyle,attr"`
+	Soap            string   `xml:"xmlns:soap,attr"`
+	Body            Body
+	HTTPContentType string `xml:"-"`
 }
 
 type Body struct {
@@ -26,12 +27,13 @@ func NewEnvelope(data interface{}) (*Envelope, error) {
 		return nil, err
 	}
 	return &Envelope{
-		Xsi:           "http://www.w3.org/2001/XMLSchema-instance",
-		Soapenc:       "http://schemas.xmlsoap.org/soap/encoding/",
-		Xsd:           "http://www.w3.org/2001/XMLSchema",
-		EncodingStyle: "http://schemas.xmlsoap.org/soap/encoding/",
-		Soap:          "http://schemas.xmlsoap.org/soap/envelope/",
-		Body:          Body{Data: string(msg)},
+		Xsi:             "http://www.w3.org/2001/XMLSchema-instance",
+		Soapenc:         "http://schemas.xmlsoap.org/soap/encoding/",
+		Xsd:             "http://www.w3.org/2001/XMLSchema",
+		EncodingStyle:   "http://schemas.xmlsoap.org/soap/encoding/",
+		Soap:            "http://schemas.xmlsoap.org/soap/envelope/",
+		Body:            Body{Data: string(msg)},
+		HTTPContentType: "text/xml; charset=utf-8", // it could also be application/soap+xml
 	}, nil
 }
 
@@ -45,30 +47,30 @@ func (env *Envelope) WriteTo(writer io.Writer) error {
 	return nil
 }
 
-func (env *Envelope) Post(url string) (error, *http.Response) {
+func (env *Envelope) Post(url string) (response *Envelope, err error) {
 	buf := new(bytes.Buffer)
-
 	buf.WriteString(`<?xml version="1.0" encoding="utf-8"?>`)
-	err := env.WriteTo(buf)
+	err = env.WriteTo(buf)
+
 	if err != nil {
-		return err, nil
+		return
 	}
 
-	r, err := http.Post(url, "application/soap+xml", buf)
+	var htresp *http.Response
+	htresp, err = http.Post(url, env.HTTPContentType, buf)
+
 	if err != nil {
-		return err, nil
-	} else {
-		return nil, r
+		return
 	}
+
+	defer htresp.Body.Close()
+
+	dec := xml.NewDecoder(htresp.Body)
+	err = dec.Decode(response)
+
+	return
 }
 
-func ReadEnvelope(reply interface{}, reader io.Reader) error {
-	var env Envelope
-
-	dec := xml.NewDecoder(reader)
-	err := dec.Decode(&env)
-	if err != nil {
-		return err
-	}
-	return xml.Unmarshal([]byte(env.Body.Data), reply)
+func (env *Envelope) Unmarshal(target interface{}) error {
+	return xml.Unmarshal([]byte(env.Body.Data), target)
 }
